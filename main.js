f536cbb374abd2c1a6a1180258caaeef068af1af
Kieran <kieran.summerfeldt@gmail.com>
2017-03-24 16:55:40 -0700

    all functionality except changing background

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 $(function() {
 
-  function getQuote(lat, lon) {
+  var temperatureKelvin;
+  var temperatureCelcius;
+  var temperatureFarenheit;
+
+  function updatePosition(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude; 
     $.ajax({
       url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=06056baaac623ac8cf8ea30e51d60e20",
       dataType: "jsonp",
       success: function(data) {
         console.log(data);
-        data;
+        $('#location').html(data.name + ", " + data.sys.country);
+        temperatureKelvin = data.main.temp;
+        toCelcius();
       }
      }); 
   }
 
-  var x = document.getElementById("demo");
   function getLocation() {
       if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(updatePosition);
@@ -19,11 +26,29 @@ $(function() {
           x.innerHTML = "Geolocation is not supported by this browser.";
       }
   }
-  function updatePosition(position) {
-      var lat = position.coords.latitude;
-      var lon = position.coords.longitude; 
-      getQuote(lat, lon);
+
+  function toCelcius() {
+    temperatureCelcius = Math.floor(temperatureKelvin - 273.15);
+    $('#temperature').html(temperatureCelcius + " &degC");
+  } 
+
+  function toFarenheit() {
+    temperatureFarenheit = Math.floor(9/5 * (temperatureKelvin - 273) + 32);
+    $('#temperature').html(temperatureFarenheit + " &degF");
   }
+  // function updatePosition(position) {
+  //     var lat = position.coords.latitude;
+  //     var lon = position.coords.longitude; 
+  //     getQuote(lat, lon);
+  // }
+
+  $('#farenheit').on('click', function() {
+    toFarenheit();
+  });
+
+  $('#celcius').on('click', function() {
+    toCelcius();
+  });
 
   getLocation()
 
